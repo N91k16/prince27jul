21297d204671398ea7af8e2588085459ad6b4315
nagendra <n91k16@gmail.com>
2025-07-27 22:21:12 +0530

    terrafform

--- child_module/rg_main.tf
+++ child_module/rg_main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "blockrg" {
+resource "azurerm_resource_group" "blockflower" {
     for_each = var.rose
     name = each.value.name
     location = each.value.location
